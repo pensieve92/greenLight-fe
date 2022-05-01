50e828c0c94d966ede3fbfce18c96ef7163ca513
byeongjeon jeon <pensieve92@gmail.com>
2022-05-01 17:00:08 +0900

    eslint 적용

--- grean-light/.eslintrc.js
+++ grean-light/.eslintrc.js
@@ -1,15 +1,40 @@
 module.exports = {
+  // 현재 eslintrc 파일을 기중으로 ESLint 규칙을 적용
   root: true,
   env: {
     node: true,
   },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+  // 추가적인 규칙들을 적용
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/essential',
+    '@vue/prettier',
+    'plugin:prettier/recommended',
+  ],
   parserOptions: {
     parser: "babel-eslint",
   },
+  // 코드 정리 플러그인 추가
+  plugins: ['prettier'],
+  // 사용자 편의 규칙 추가
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    'prettier/prettier': [
+      'error',
+      // 아래 규칙들은 개인 선호에 따라 prettier 문법 적용
+      // https://prettier.io/docs/en/options.html
+      {
+        singleQuote: true,
+        semi: true,
+        useTabs: true,
+        tabWidth: 2,
+        trailingComma: 'all',
+        printWidth: 80,
+        bracketSpacing: true,
+        arrowParens: 'avoid',
+      },
+    ],
   },
   overrides: [
     {
